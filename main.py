deb555dae66801aead745cf2ba56ef5cd3467574
Dlawlet <Daremol@protonmail.com>
2022-06-01 01:05:11 +0200

    version1

---
+++ main.py
@@ -0,0 +1,58 @@
+import datetime
+import os
+import random
+import shutil
+import numpy as np
+import cv2
+from glob import glob
+from fpdf import FPDF
+
+def create_dir(path):
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"ERROR: creating directoring with name {path}")
+
+def remove_dir(path):
+    try: 
+        if  os.path.exists(path):
+            shutil.rmtree(path)
+    except OSError:
+        print(f"ERROR: deleting directoring with name {path}")
+             
+def save_frame(video_path, save_dir, gap):
+    name = video_path.split("/")[-1].split(".")[0]
+    save_path =  os.path.join(save_dir, name)
+    create_dir(save_path)
+
+    pdf = FPDF()
+
+    cap =cv2.VideoCapture(video_path)
+    idx = 0
+
+    cperpage = 0
+    perpage = 2
+    pdf.add_page()
+
+    while idx<=3000:
+        ret, frame = cap.read()
+
+        if ret == False:
+            cap.release()
+            break
+        if idx % gap == 0:
+            cv2.imwrite(f"{save_path}/{idx}.png", frame)
+            if cperpage == perpage:
+                pdf.add_page()
+                cperpage = 0
+            pdf.image(f"{save_path}/{idx}.png",10,cperpage*120+10,190,100)
+            cperpage+=1
+        
+        idx += 1
+    remove_dir(save_path)
+    pdf.output(f"{name}_{random.randint(0,123345)}.pdf", "F")
+if __name__ == "__main__" :
+    video_paths = "videos/Mbampromo1.mp4"
+    save_dir = "save"
+    save_frame(video_paths, save_dir, gap = 1000)
